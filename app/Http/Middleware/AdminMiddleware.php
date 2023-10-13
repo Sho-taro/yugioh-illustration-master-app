b2691fcd58e727a0e566994ea103903588f23fbe
Sho-taro <sho96taro.biz@gmail.com>
2023-10-13 21:37:09 +0900

    AdminMiddleware.phpを一部修正

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Access\AuthorizationException;
 
 class AdminMiddleware
@@ -19,8 +20,12 @@ class AdminMiddleware
 
         // ココに処理を書いていく！!
         $user = $request->user();  // Requestクラスのuser関数で、アクセスしてきたuserを取得
-        if ($user === null || $user->id !== 1 || $user->name !== 'skkch') {
-            throw new AuthorizationException('アクセス権限がありません');
+
+        if ($user === null) {
+            throw new AuthenticationException('未ログインです');  // 未ログインの場合は、 401 unauthorized エラーをthrowする
+        }
+        if ($user->id !== 1 || $user->name !== 'skkch') {
+            throw new AuthorizationException('アクセス権限がありません');  // 一般ユーザ（管理ユーザでない）場合は、403 forbidden エラーをthrowする
         }
         // echo 'authミドルウェア';
 
