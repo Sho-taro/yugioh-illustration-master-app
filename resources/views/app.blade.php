93f98adaea5afc6c19fd7b982435a1c9567cb3e9
Sho-taro <sho96taro.biz@gmail.com>
2023-10-13 12:07:05 +0900

    resources/views/app.blade.php にリセットcssの読み込みを追加 public/js/anime.min.js を追加 resources/views/app.blade.php にanime.min.jsファイルの読み込みを追加

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- ↓ リセットcss -->
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <!-- ↓ anime.js -->
+        <script src="/js/anime.min.js" defer></script>
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
