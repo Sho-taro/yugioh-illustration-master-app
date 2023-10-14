a6ff996dc74b60fd915252bca210f10e08a89b1a
Sho-taro <sho96taro.biz@gmail.com>
2023-10-14 20:12:12 +0900

    新規ユーザ登録・ログイン画面を日本語化 レイアウトを調整

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -27,71 +27,74 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Log in" />
+		<GuestLayout>
+			<Head title="ログイン" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+			{status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+			<form onSubmit={submit}>
+				<div>
+					<InputLabel htmlFor="email" value="メールアドレス" />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
+					<TextInput
+						id="email"
+						type="email"
+						name="email"
+						value={data.email}
+						className="mt-1 block w-full"
+						autoComplete="username"
+						isFocused={true}
+						onChange={e => setData('email', e.target.value)}
+					/>
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+					<InputError message={errors.email} className="mt-2" />
+				</div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+				<div className="mt-4">
+					<InputLabel htmlFor="password" value="パスワード" />
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
+					<TextInput
+						id="password"
+						type="password"
+						name="password"
+						value={data.password}
+						className="mt-1 block w-full"
+						autoComplete="current-password"
+						onChange={e => setData('password', e.target.value)}
+					/>
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
+					<InputError message={errors.password} className="mt-2" />
+				</div>
 
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ml-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
+				<div className="block mt-4">
+					<label className="flex items-center">
+						<Checkbox
+							name="remember"
+							checked={data.remember}
+							onChange={e => setData('remember', e.target.checked)}
+						/>
+						<span className="ml-2 text-sm text-gray-600">ログイン状態を保持する</span>
+					</label>
+				</div>
 
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
+				<div className="flex items-center justify-between mt-4">
+					<PrimaryButton className="ml-4" disabled={processing}>
+						ログイン
+					</PrimaryButton>
+					{/* {canResetPassword && (
                         <Link
                             href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
+                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            パスワードをお忘れですか？
                         </Link>
-                    )}
-
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
-    );
+                    )} */}
+					<Link
+						href={route('register')}
+						className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+						まだ登録がお済みでない方はこちら
+					</Link>
+				</div>
+			</form>
+		</GuestLayout>
+	);
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -27,91 +27,89 @@ export default function Register() {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Register" />
+		<GuestLayout>
+			<Head title="新規登録" />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="name" value="Name" />
+			<form onSubmit={submit}>
+				<div>
+					<InputLabel htmlFor="name" value="ユーザー名" />
 
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData('name', e.target.value)}
-                        required
-                    />
+					<TextInput
+						id="name"
+						name="name"
+						value={data.name}
+						className="mt-1 block w-full"
+						autoComplete="name"
+						isFocused={true}
+						onChange={e => setData('name', e.target.value)}
+						required
+					/>
 
-                    <InputError message={errors.name} className="mt-2" />
-                </div>
+					<InputError message={errors.name} className="mt-2" />
+				</div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
+				<div className="mt-4">
+					<InputLabel htmlFor="email" value="メールアドレス" />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
-                    />
+					<TextInput
+						id="email"
+						type="email"
+						name="email"
+						value={data.email}
+						className="mt-1 block w-full"
+						autoComplete="username"
+						onChange={e => setData('email', e.target.value)}
+						required
+					/>
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
+					<InputError message={errors.email} className="mt-2" />
+				</div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+				<div className="mt-4">
+					<InputLabel htmlFor="password" value="パスワード（半角英数字8文字以上）" />
 
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                        required
-                    />
+					<TextInput
+						id="password"
+						type="password"
+						name="password"
+						value={data.password}
+						className="mt-1 block w-full"
+						autoComplete="new-password"
+						onChange={e => setData('password', e.target.value)}
+						required
+					/>
 
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
+					<InputError message={errors.password} className="mt-2" />
+				</div>
 
-                <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+				<div className="mt-4">
+					<InputLabel htmlFor="password_confirmation" value="パスワード再入力" />
 
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
-                        required
-                    />
+					<TextInput
+						id="password_confirmation"
+						type="password"
+						name="password_confirmation"
+						value={data.password_confirmation}
+						className="mt-1 block w-full"
+						autoComplete="new-password"
+						onChange={e => setData('password_confirmation', e.target.value)}
+						required
+					/>
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
+					<InputError message={errors.password_confirmation} className="mt-2" />
+				</div>
 
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ml-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
-    );
+				<div className="flex items-center justify-between mt-4">
+					<PrimaryButton className="ml-4" disabled={processing}>
+						新規登録
+					</PrimaryButton>
+					<Link
+						href={route('login')}
+						className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+						既に登録済みの方はこちら
+					</Link>
+				</div>
+			</form>
+		</GuestLayout>
+	);
 }
