1aa17d949fc9b4d294d9d598b30c26415de0a41d
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-11-24 07:04:17 +0900

    [add]Add column definition
    refs #25 cards2テーブルのマイグレーションファイルに、カラム定義を記述しました

--- database/migrations/2023_11_23_215027_create_cards2_table.php
+++ database/migrations/2023_11_23_215027_create_cards2_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('cards2', function (Blueprint $table) {
             $table->id();
+            $table->string('product_code');
+            $table->foreign('product_code')->references('product_code')->on('products')->cascadeOnDelete();   // 外部キー制約
+            $table->string('list_number');
+            $table->string('card_id');
+            $table->string('name_en');
+            $table->string('name_ja');
+            $table->string('name_ja_kana');
+            $table->string('frame_type_code');
+            $table->foreign('frame_type_code')->references('frame_type_code')->on('frame_types')->cascadeOnDelete();  //外部キー制約
+            $table->string('archetype')->nullable();
             $table->timestamps();
         });
     }
