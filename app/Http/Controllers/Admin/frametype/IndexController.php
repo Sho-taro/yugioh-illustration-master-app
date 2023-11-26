6077915bed65cdbadcfe7a5788d36b5a5781c5c4
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-11-26 17:42:20 +0900

    [add]Add index page
    refs #35 一覧表示ページのファイルを作成しました（一覧表示機能自体は未実装）。

--- app/Http/Controllers/Admin/frametype/IndexController.php
+++ app/Http/Controllers/Admin/frametype/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\frametype;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class IndexController extends Controller
 {
@@ -12,6 +13,6 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        return inertia('Admin/FrameType/Index', []);
     }
 }
---
+++ resources/js/Pages/Admin/FrameType/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <>
+      <p>frame_typesの一覧表示ページ</p>
+    </>
+  )
+}
+
+export default Index
