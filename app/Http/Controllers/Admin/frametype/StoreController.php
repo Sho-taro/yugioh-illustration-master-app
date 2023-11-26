7c06de3c5ed7a49e39f1f8c52eb7038e153582a7
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-11-27 01:40:29 +0900

    [add]Add register logic
    refs #35 新規frame_typeを登録するロジックを記述しました。また、バリデーションの設定も行いました。

--- app/Http/Controllers/Admin/frametype/StoreController.php
+++ app/Http/Controllers/Admin/frametype/StoreController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\frametype;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\FrameType;
+use Inertia\Inertia;
 
 class StoreController extends Controller
 {
@@ -12,6 +14,16 @@ class StoreController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        FrameType::create($request->validate([
+            'frame_type_code' => ['required', 'string', 'min:3', 'max:3', 'unique:frame_types,frame_type_code'],
+            'name_en' => ['required', 'string', 'unique:frame_types,name_en'],
+            'name_ja' => ['required', 'string', 'unique:frame_types,name_ja']
+        ]));
+
+        // $data = Card::orderBy('created_at', 'DESC')->paginate(15);    // paginateメソッドは、配列ではなくコレクション（jsonオブジェクト？）を返す
+        $registeredFrameType = FrameType::orderBy('created_at', 'DESC')->firstOrFail();
+
+        // return redirect('/admin/card');
+        return inertia('Admin/FrameType/Create', ['registeredFrameType' => $registeredFrameType, 'message' => 'frame_typeを新規登録しました']);
     }
 }
