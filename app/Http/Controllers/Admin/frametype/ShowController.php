5ec844b7117e6de0694390e179a25eb3c71a5fa2
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-11-27 03:50:55 +0900

    [add]Add ShowController logic
    refs #35 ShowControllerのロジックを記述しました。

--- app/Http/Controllers/Admin/frametype/ShowController.php
+++ app/Http/Controllers/Admin/frametype/ShowController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin\frametype;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\FrameType;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Inertia\Inertia;
 
 class ShowController extends Controller
 {
@@ -12,6 +15,17 @@ class ShowController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        //URLのクエリパラメータからidを取得
+        $id = (int)$request->route('id');
+
+        //idが一致するframe_typeレコードをDBから取得
+        $frame_type = FrameType::where('id', $id)->firstOrFail();
+
+        //もし一致するカードが見つからなければエラー
+        if(is_null($frame_type)) {
+            throw new NotFoundHttpException('該当するframe_typeが見つかりませんでした');
+        }
+
+        return inertia('Admin/FrameType/Show', ['frameType' => $frame_type]);
     }
 }
