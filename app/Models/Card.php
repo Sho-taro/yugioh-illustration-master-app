9890ec9f91705c3404b6ef7018280325bc9c2c9c
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-04 08:47:23 +0900

    [add]Add Models relation
    refs #72 モデル間のリレーションを追加しました。

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;  // 追加
 
 class Card extends Model
 {
@@ -27,4 +28,17 @@ class Card extends Model
         'frame_type',
         'archetype',
     ];
+
+    public function product() :BelongsTo
+    {
+        // Productsテーブルの'product_code'カラムから、自身（Cardsテーブル）の'product_code'カラムと一致する値を検索して関連レコードを取得する
+        return $this->belongsTo(Product::class, 'product_code', 'product_code');
+    }
+
+    public function frameType() :BelongsTo
+    {
+        // ＊これだけ第２引数と第３引数の順番が逆
+        // 自身(Cardsテーブル)の'frame_type'カラムと一致する値を、frame_typesテーブルの'name_en'カラムから検索して関連レコードを取得する
+        return $this->belongsTo(FrameType::class, 'frame_type', 'name_en');
+    }
 }
--- app/Models/FrameType.php
+++ app/Models/FrameType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;   // 追加
 
 class FrameType extends Model
 {
@@ -20,4 +21,10 @@ class FrameType extends Model
         'name_en',
         'name_ja',
     ];
+
+    public function cards() :HasMany
+    {
+        // cardsテーブルの'frame_type'カラムから、自身（frame_typesテーブル）の'name_en'カラムと一致する値を検索して関連レコードを取得する
+        return $this->hasMany(Card::class, 'frame_type', 'name_en');
+    }
 }
