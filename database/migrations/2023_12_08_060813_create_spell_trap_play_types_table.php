a71080435507511045f19d2482c3caa4751e9e0d
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-08 15:09:28 +0900

    [add]Add 'spell_trap_play_types' migration file
    refs #79 spell_trap_play_typesテーブルのマイグレーションファイルを新規作成

---
+++ database/migrations/2023_12_08_060813_create_spell_trap_play_types_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spell_trap_play_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('play_type_code', 8)->unique();
+            $table->string('name_ja', 100);
+            $table->string('name_en', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spell_trap_play_types');
+    }
+};
