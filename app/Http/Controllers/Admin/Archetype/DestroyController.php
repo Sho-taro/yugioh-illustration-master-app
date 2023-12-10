ed0deae227902ee8f204babb8fa977bd90d371c9
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-10 13:01:42 +0900

    [add]Add Destroy feature
    refs #96 削除機能を追加

--- app/Http/Controllers/Admin/Archetype/DestroyController.php
+++ app/Http/Controllers/Admin/Archetype/DestroyController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\Archetype;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Archetype;
+use Inertia\Inertia;
 
 class DestroyController extends Controller
 {
@@ -12,6 +14,22 @@ class DestroyController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        // URLのクエリパラメータからidを取得
+        $id = (int)$request->route('id');
+
+        // 取得したidを持つarchetypeをDBから取得
+        $archetype = Archetype::where('id', $id)->firstOrFail();
+
+        //もし一致するarchetypeが見つからなければエラー
+        if(is_null($archetype)) {
+            throw new NotFoundHttpException('該当するarchetypeが見つかりませんでした');
+        }
+
+        // 該当するarchetypeをDBから削除
+        $archetype->delete();
+
+        $data = Archetype::orderBy('updated_at', 'DESC')->paginate(15);
+
+        return inertia('Admin/Archetype/Index', ['data' => $data, 'message' => "id: {$id} の archetype を削除しました"]);
     }
 }
