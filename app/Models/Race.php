1b24d7bf2b59cf9cfea8e62bbc806300c3114ff1
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-10 14:20:54 +0900

    [add]Add Model
    refs #98 Raceモデルを追加

--- app/Models/Race.php
+++ app/Models/Race.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Race extends Model
 {
     use HasFactory;
+
+    // racesテーブルに明示的に紐付けする
+    protected $table = 'races';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'race_code',
+        'name_ja',
+        'name_en',
+    ];
 }
