3f8c82eaacf676cbea4bf1f620ac0d4e46a67da3
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-10 14:33:01 +0900

    [add]Add Destroy feature
    refs #98 削除機能を追加

--- app/Http/Controllers/Admin/Race/DestroyController.php
+++ app/Http/Controllers/Admin/Race/DestroyController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\Race;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Race;
+use Inertia\Inertia;
 
 class DestroyController extends Controller
 {
@@ -12,6 +14,22 @@ class DestroyController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        // URLのクエリパラメータからidを取得
+        $id = (int)$request->route('id');
+
+        // 取得したidを持つraceをDBから取得
+        $race = Race::where('id', $id)->firstOrFail();
+
+        //もし一致するraceが見つからなければエラー
+        if(is_null($race)) {
+            throw new NotFoundHttpException('該当するraceが見つかりませんでした');
+        }
+
+        // 該当するraceをDBから削除
+        $race->delete();
+
+        $data = Race::orderBy('updated_at', 'DESC')->paginate(15);
+
+        return inertia('Admin/Race/Index', ['data' => $data, 'message' => "id: {$id} の race を削除しました"]);
     }
 }
