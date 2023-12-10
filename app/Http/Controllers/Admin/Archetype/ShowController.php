9b1fee15d4a34331d1928f7cfe7461f37378d280
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-10 12:58:32 +0900

    [add]Add Show and Update features
    refs #96 詳細表示機能と更新機能を追加

--- app/Http/Controllers/Admin/Archetype/ShowController.php
+++ app/Http/Controllers/Admin/Archetype/ShowController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin\Archetype;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Archetype;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Inertia\Inertia;
 
 class ShowController extends Controller
 {
@@ -12,6 +15,17 @@ class ShowController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        //URLのクエリパラメータからidを取得
+        $id = (int)$request->route('id');
+
+        //idが一致するarchetypeレコードをDBから取得
+        $archetype = Archetype::where('id', $id)->firstOrFail();
+
+        //もし一致するカードが見つからなければエラー
+        if(is_null($archetype)) {
+            throw new NotFoundHttpException('該当するarchetypeが見つかりませんでした');
+        }
+
+        return inertia('Admin/Archetype/Show', ['archetype' => $archetype]);
     }
 }
--- app/Http/Controllers/Admin/Archetype/UpdateController.php
+++ app/Http/Controllers/Admin/Archetype/UpdateController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin\Archetype;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Archetype;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Inertia\Inertia;
 
 class UpdateController extends Controller
 {
@@ -12,6 +15,31 @@ class UpdateController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        // URLのクエリパラメータからidを取得
+        $id = (int)$request->route('id');
+
+        // 取得したidを持つarchetypeをDBから取得
+        $archetype = Archetype::where('id', $id)->firstOrFail();
+
+        //もし一致するarchetypeが見つからなければエラー
+        if(is_null($archetype)) {
+            throw new NotFoundHttpException('該当するarchetypeが見つかりませんでした');
+        }
+
+        // フォームに入力された変更内容をバリデーション
+        $updated_archetype = $request->validate([
+            // 更新処理時のバリデーションチェックでは、unique制約はつけてはいけない
+            'archetype_code' => ['required', 'string', 'size:4'],
+            'name_ja' => ['required', 'string'],
+            'name_en' => ['required', 'string']
+        ]);
+        // dd($updated_archetype);
+
+        // バリデーションエラーがなければ、変更内容を保存
+        $archetype->update($updated_archetype);
+
+        $data = Archetype::orderBy('updated_at', 'DESC')->paginate(15);
+
+        return inertia('Admin/Archetype/Index', ['data' => $data, 'message' => "id: {$id} の archetype を編集しました"]);
     }
 }
