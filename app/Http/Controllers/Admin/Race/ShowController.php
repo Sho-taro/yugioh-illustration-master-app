c81f906f300088e9697620e5f59e694137bcf1ce
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-10 14:30:44 +0900

    [add]Add Show and Update features
    refs #98 詳細表示機能と編集機能を追加

--- app/Http/Controllers/Admin/Race/ShowController.php
+++ app/Http/Controllers/Admin/Race/ShowController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin\Race;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Race;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Inertia\Inertia;
 
 class ShowController extends Controller
 {
@@ -12,6 +15,17 @@ class ShowController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        //URLのクエリパラメータからidを取得
+        $id = (int)$request->route('id');
+
+        //idが一致するraceレコードをDBから取得
+        $race = Race::where('id', $id)->firstOrFail();
+
+        //もし一致するカードが見つからなければエラー
+        if(is_null($race)) {
+            throw new NotFoundHttpException('該当するraceが見つかりませんでした');
+        }
+
+        return inertia('Admin/Race/Show', ['race' => $race]);
     }
 }
--- app/Http/Controllers/Admin/Race/UpdateController.php
+++ app/Http/Controllers/Admin/Race/UpdateController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin\Race;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Race;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Inertia\Inertia;
 
 class UpdateController extends Controller
 {
@@ -12,6 +15,31 @@ class UpdateController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        // URLのクエリパラメータからidを取得
+        $id = (int)$request->route('id');
+
+        // 取得したidを持つraceをDBから取得
+        $race = Race::where('id', $id)->firstOrFail();
+
+        //もし一致するraceが見つからなければエラー
+        if(is_null($race)) {
+            throw new NotFoundHttpException('該当するraceが見つかりませんでした');
+        }
+
+        // フォームに入力された変更内容をバリデーション
+        $updated_race = $request->validate([
+            // 更新処理時のバリデーションチェックでは、unique制約はつけてはいけない
+            'race_code' => ['required', 'string', 'size:4'],
+            'name_ja' => ['required', 'string'],
+            'name_en' => ['required', 'string']
+        ]);
+        // dd($updated_race);
+
+        // バリデーションエラーがなければ、変更内容を保存
+        $race->update($updated_race);
+
+        $data = Race::orderBy('updated_at', 'DESC')->paginate(15);
+
+        return inertia('Admin/Race/Index', ['data' => $data, 'message' => "id: {$id} の race を編集しました"]);
     }
 }
