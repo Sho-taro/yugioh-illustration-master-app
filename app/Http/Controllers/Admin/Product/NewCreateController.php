c100269876fd1203f33ba127597f9ec1a0fa4fbe
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-20 03:23:31 +0900

    [add]Add new store feature
    refs #115 productをjavascript配列をもとに新規作成する機能を実装

---
+++ app/Http/Controllers/Admin/Product/NewCreateController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Product;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class NewCreateController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return inertia('Admin/Product/NewCreate', []);
+    }
+}
---
+++ resources/js/utils/productCodes/basicBoosterPacks.js
@@ -0,0 +1,132 @@
+export const basicBoosterPacks = [
+	['vol1', 'vol. 1', '', '1999-02-04'],
+	['vol2', 'vol. 2', '', '1999-03-27'],
+	['vol3', 'vol. 3', '', '1999-05-27'],
+	['vol4', 'vol. 4', '', '1999-07-22'],
+	['vol5', 'vol. 5', '', '1999-09-23'],
+	['vol6', 'vol. 6', '', '1999-11-18'],
+	['vol7', 'vol. 7', '', '2000-01-27'],
+	['mr', '— 魔法の支配者 —', 'magic ruler', '2000-04-20'],
+	['ps', '— ファラオのしもべ —', 'pharaoh’s servant', '2000-07-13'],
+	['ca', '— アヌビスの呪い —', 'curse of anubis', '2000-09-28'],
+	['tb', '— 千眼の魔術書 —', 'thousand eyes bible', '2000-12-14'],
+	['sm', '— 仮面の呪縛 —', 'spell of mask', '2001-04-19'],
+	['ln', '— 悪夢の迷宮 —', 'labyrinth of nightmare', '2001-07-12'],
+	['sc', '— 闇を制する者 —', 'struggle of chaos', '2001-09-20'],
+	['ma', '— 蘇りし魂 —', 'mythological age', '2001-11-29'],
+	['ph', '— 王家の守護者 —', 'pharaonic guardian', '2002-03-21'],
+	['301', '新たなる支配者', '', '2002-05-16'],
+	['302', 'ユニオンの降臨', '', '2002-07-18'],
+	['303', '黒魔導の覇者', '', '2002-09-19'],
+	['304', 'ガーディアンの力', '', '2002-11-21'],
+	['305', '闇魔界の脅威', '', '2003-02-20'],
+	['306', '混沌を制す者', '', '2003-04-24'],
+	['307', '暗黒の侵略者', '', '2003-07-17'],
+	['308', '天空の聖域', '', '2003-11-20'],
+	['309', 'ファラオの遺産', '', '2004-02-26'],
+	['sod', 'ソウル・オブ・ザ・デュエリスト', 'soul of the duelist', '2004-05-27'],
+	['rds', 'ライズ・オブ・デスティニー', 'rise of destiny', '2004-08-05'],
+	['fet', 'フレイミング・エターニティ', 'flaming eternity', '2004-11-25'],
+	['tlm', 'ザ・ロスト・ミレニアム', 'the lost millennium', '2005-02-24'],
+	['crv', 'サイバネティック・レボリューション', 'cybernetic revolution', '2005-05-26'],
+	['een', 'エレメンタル・エナジー', 'elemental energy', '2005-08-11'],
+	['soi', 'シャドウ・オブ・インフィニティ', 'shadow of infinity', '2005-11-17'],
+	['eoj', 'エネミー・オブ・ジャスティス', 'enemy of justice', '2006-02-16'],
+	['potd', 'パワー・オブ・ザ・デュエリスト', 'power of the duelist', '2006-05-18'],
+	['cdip', 'サイバーダーク・インパクト', 'cyberdark impact', '2006-08-10'],
+	['ston', 'ストライク・オブ・ネオス', 'strike of neos', '2006-11-16'],
+	['fotb', 'フォース・オブ・ザ・ブレイカー', 'force of the breaker', '2007-02-15'],
+	['taev', 'タクティカル・エボリューション', 'tactical evolution', '2007-05-12'],
+	['glas', 'グラディエイターズ・アサルト', 'gladiator’s assault', '2007-07-21'],
+	['ptdn', 'ファントム・ダークネス', 'phantom darkness', '2007-11-23'],
+	['lodt', 'ライト・オブ・デストラクション', 'light of destruction', '2008-02-23'],
+	['tdgs', 'ザ・デュエリスト・ジェネシス', 'the duelist genesis', '2008-04-19'],
+	['csoc', 'クロスローズ・オブ・カオス', 'crossroads of chaos', '2008-07-19'],
+	['crms', 'クリムゾン・クライシス', 'crimson crisis', '2008-11-15'],
+	['rgbt', 'レイジング・バトル', 'raging battle', '2009-02-14'],
+	['anpr', 'エンシェント・プロフェシー', 'ancient prophecy', '2009-04-18'],
+	['sovr', 'スターダスト・オーバードライブ', 'stardust overdrive', '2009-07-18'],
+	['abpf', 'アブソリュート・パワーフォース', 'absolute powerforce', '2009-11-14'],
+	['tshd', 'ザ・シャイニング・ダークネス', 'the shining darkness', '2010-02-20'],
+	['drev', 'デュエリスト・レボリューション', 'duelist revolution', '2010-04-17'],
+	['stbl', 'スターストライク・ブラスト', 'starstrike blast', '2010-07-17'],
+	['stor', 'ストーム・オブ・ラグナロク', 'storm of ragnarok', '2010-11-13'],
+	['exvc', 'エクストリーム・ビクトリー', 'extreme victory', '2011-02-11'],
+	['genf', 'ジェネレーション・フォース', 'generation force', '2011-04-16'],
+	['phsw', 'フォトン・ショックウェーブ', 'photon shockwav', '2011-07-16'],
+	['orcs', 'オーダー・オブ・カオス', 'order of chaos', '2011-11-19'],
+	['gaov', 'ギャラクティック・オーバーロード', 'galactic overload', '2012-02-18'],
+	['redu', 'リターン・オブ・ザ・デュエリスト', 'return of the duelist', '2012-04-14'],
+	['abyr', 'アビス・ライジング', 'abyss rising', '2012-07-21'],
+	['cblz', 'コスモ・ブレイザー', 'cosmo blazer', '2012-11-17'],
+	['ltgy', 'ロード・オブ・ザ・タキオンギャラクシー', 'lord of the tachyon galaxy', '2013-02-16'],
+	['jotl', 'ジャッジメント・オブ・ザ・ライト', 'judgment of the light', '2013-04-20'],
+	['shsp', 'シャドウ・スペクターズ', 'shadow specters', '2013-07-20'],
+	['lval', 'レガシー・オブ・ザ・ヴァリアント', 'legacy of the valiant', '2013-11-16'],
+	['prio', 'プライマル・オリジン', 'primal origin', '2014-02-15'],
+	['duea', 'ザ・デュエリスト・アドベント', 'the duelist advent', '2014-04-19'],
+	['nech', 'ネクスト・チャレンジャーズ', 'next challengers', '2014-07-19'],
+	['sece', 'ザ・シークレット・オブ・エボルーション', 'the secret of evolution', '2014-11-15'],
+	['cros', 'クロスオーバー・ソウルズ', 'crossover souls', '2015-02-14'],
+	['core', 'クラッシュ・オブ・リベリオン', 'clash of rebellions', '2015-04-25'],
+	['docs', 'ディメンション・オブ・カオス', 'dimension of chaos', '2015-07-18'],
+	['bosh', 'ブレイカーズ・オブ・シャドウ', 'breakers of shadow', '2015-10-17'],
+	['shvi', 'シャイニング・ビクトリーズ', 'shining victories', '2016-01-09'],
+	['tdil', 'ザ・ダーク・イリュージョン', 'the dark illusion', '2016-04-09'],
+	['inov', 'インベイジョン・オブ・ヴェノム', 'invasion of venom', '2016-07-09'],
+	['rate', 'レイジング・テンペスト', 'raging tempest', '2016-10-08'],
+	['macr', 'マキシマム・クライシス', 'maximum crisis', '2017-01-14'],
+	['cotd', 'コード・オブ・ザ・デュエリスト', 'code of the duelist', '2017-04-15'],
+	['cibr', 'サーキット・ブレイク', 'circuit break', '2017-07-08'],
+	['exfo', 'エクストリーム・フォース', 'extreme force', '2017-10-14'],
+	['flod', 'フレイムズ・オブ・デストラクション', 'flames of destruction', '2018-01-13'],
+	['cyho', 'サイバネティック・ホライゾン', 'cybernetic horizon', '2018-04-14'],
+	['sofu', 'ソウル・フュージョン', 'soul fusion', '2018-07-14'],
+	['sast', 'サベージ・ストライク', 'savage strike', '2018-10-13'],
+	['dane', 'ダーク・ネオストーム', 'dark neostorm', '2019-01-12'],
+	['rira', 'ライジング・ランペイジ', 'rising rampage', '2019-04-13'],
+	['chim', 'カオス・インパクト', 'chaos impact', '2019-07-13'],
+	['igas', 'イグニッション・アサルト', 'ignition assault', '2019-10-12'],
+	['etco', 'エターニティ・コード', 'eternity code', '2020-01-11'],
+	['rotd', 'ライズ・オブ・ザ・デュエリスト', 'rise of the duelist', '2020-04-18'],
+	['phra', 'ファントム・レイジ', 'phantom rage', '2020-08-08'],
+	['blvo', 'ブレイジング・ボルテックス', 'blazing vortex', '2020-10-31'],
+	['liov', 'ライトニング・オーバードライブ', 'lightning overdrive', '2021-01-16'],
+	['dama', 'ドーン・オブ・マジェスティ', 'dawn of majesty', '2021-04-17'],
+	['bode', 'バースト・オブ・ディスティニー', 'burst of destiny', '2021-07-17'],
+	['bach', 'バトル・オブ・カオス', 'battle of chaos', '2021-10-16'],
+	['difo', 'ディメンション・フォース', 'dimension force', '2022-01-15'],
+	['pote', 'パワー・オブ・ジ・エレメンツ', 'power of the elements', '2022-04-23'],
+	['dabl', 'ダークウィング・ブラスト', 'darkwing blast', '2022-07-16'],
+	['phhy', 'フォトン・ハイパーノヴァ', 'photon hypernova', '2022-10-15'],
+	['cyac', 'サイバーストーム・アクセス', 'cyberstorm access', '2023-01-14'],
+	['dune', 'デュエリスト・ネクサス', 'duelist nexus', '2023-04-22'],
+	['agov', 'エイジ・オブ・オーバーロード', 'age of overload', '2023-07-22'],
+	['phni', 'ファントム・ナイトメア', 'phantom nightmare', '2023-10-28'],
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
