ad3edafb6c9db5c024d8db859b7059c4822a4be2
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-19 19:27:05 +0900

    [add]Add destroy feature
    refs #113 削除機能を実装

--- app/Http/Controllers/Admin/ReleasedCard/DestroyController.php
+++ app/Http/Controllers/Admin/ReleasedCard/DestroyController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\ReleasedCard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ReleasedCard;
+use Inertia\Inertia;
 
 class DestroyController extends Controller
 {
@@ -12,6 +14,23 @@ class DestroyController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        // URLのクエリパラメータからidを取得
+        $id = (int)$request->route('id');
+
+        // 取得したidを持つreleased_cardをDBから取得
+        $released_card = ReleasedCard::where('id', $id)->firstOrFail();
+
+        //もし一致するreleased_cardが見つからなければエラー
+        if(is_null($released_card)) {
+            throw new NotFoundHttpException('該当するreleased_cardが見つかりませんでした');
+        }
+
+        // 該当するreleased_cardをDBから削除
+        $released_card->delete();
+
+        $data = ReleasedCard::orderBy('updated_at', 'DESC')->paginate(15);
+
+        return inertia('Admin/ReleasedCard/Index', ['data' => $data, 'message' => "id: {$id} の released_card を削除しました"]);
+
     }
 }
