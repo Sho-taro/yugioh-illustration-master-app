c29af1e9ee29fe759cae61a14cbff096f1a7ac37
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-23 03:49:19 +0900

    [add]Add URL
    refs #119 cardの詳細ページから対応するreleased_cardの詳細ページに飛べるURLを追加

--- resources/js/Components/Admin/ReleasedCardsList.jsx
+++ resources/js/Components/Admin/ReleasedCardsList.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from '@inertiajs/react';
 
 function ReleasedCardsList({releasedCards}) {
   return (
@@ -6,6 +7,7 @@ function ReleasedCardsList({releasedCards}) {
 			<thead>
 				<tr>
 					<th className="w-40">イラスト</th>
+					<th className="w-12">id</th>
 					<th className="w-40">型番</th>
 					<th className="w-80">収録</th>
 					<th className="w-40">発売日</th>
@@ -13,14 +15,21 @@ function ReleasedCardsList({releasedCards}) {
 				</tr>
 			</thead>
 			{releasedCards.map((rc, index) => (
-        <tbody key={index}>
+				<tbody key={index}>
 					<tr>
 						<td>
 							<img
 								src={`/images/card-images/${rc.product_code}-${rc.list_number}.jpg`}
-                className="w-32"
+								className="w-32"
 								alt="イラスト"
 							/>
+						</td>
+						<td>
+							<Link
+								href={`/admin/releasedcard/${rc.id}`}
+								className="hover:text-blue-400 underline">
+								{rc.id}
+							</Link>
 						</td>
 						<td>{`${rc.product_code}-${rc.list_number}`}</td>
 						<td>{rc.name_ja}</td>
