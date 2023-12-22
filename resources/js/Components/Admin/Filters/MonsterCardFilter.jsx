e6d4a59c5b7e2aecdeb3af8abc10626dbc018321
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-22 09:52:15 +0900

    [add]Add new filters
    refs #113 レベル/ランクfilterとLink-filterを追加

--- resources/js/Components/Admin/Filters/MonsterCardFilter.jsx
+++ resources/js/Components/Admin/Filters/MonsterCardFilter.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import FrameTypeFilter from './FrameTypeFilter';
 import RaceFilter from './RaceFilter';
 import AttributeFilter from './AttributeFilter';
+import LevelOrRankFilter from './LevelOrRankFilter';
+import LinkValueFilter from './LinkValueFilter';
 
 function MonsterCardFilter() {
 	return (
 		<>
 			<FrameTypeFilter />
 			<RaceFilter />
-			<AttributeFilter/>
+			<AttributeFilter />
+			<LevelOrRankFilter />
+			<LinkValueFilter />
 		</>
 	);
 }
