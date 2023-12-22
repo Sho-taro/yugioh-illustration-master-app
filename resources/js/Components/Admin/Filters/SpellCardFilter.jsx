413d8e03f01f306b7f23a83bfce19ed3dacfa849
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2023-12-23 00:58:04 +0900

    [modify]Modify input name
    refs #113 input(checkbox)のname属性を修正

--- resources/js/Components/Admin/Filters/SpellCardFilter.jsx
+++ resources/js/Components/Admin/Filters/SpellCardFilter.jsx
@@ -5,27 +5,27 @@ function SpellCardFilter() {
 		<div className="mt-2 flex justify-start items-center">
 			<p className="w-28 text-center">種類</p>
 			<div className="ml-2">
-				<input type="checkbox" id="Normal" name="frame-types[]" value="Normal" />
+				<input type="checkbox" id="Normal" name="play-types[]" value="Normal" />
 				<label htmlFor="Normal" className="mr-4 select-none">
 					通常魔法
 				</label>
-				<input type="checkbox" id="Field" name="frame-types[]" value="Field" />
+				<input type="checkbox" id="Field" name="play-types[]" value="Field" />
 				<label htmlFor="Field" className="mr-4 select-none">
 					フィールド魔法
 				</label>
-				<input type="checkbox" id="Equip" name="frame-types[]" value="Equip" />
+				<input type="checkbox" id="Equip" name="play-types[]" value="Equip" />
 				<label htmlFor="Equip" className="mr-4 select-none">
 					装備魔法
 				</label>
-				<input type="checkbox" id="Continuous" name="frame-types[]" value="Continuous" />
+				<input type="checkbox" id="Continuous" name="play-types[]" value="Continuous" />
 				<label htmlFor="Continuous" className="mr-4 select-none">
 					永続魔法
 				</label>
-				<input type="checkbox" id="Quick-Play" name="frame-types[]" value="Quick-Play" />
+				<input type="checkbox" id="Quick-Play" name="play-types[]" value="Quick-Play" />
 				<label htmlFor="Quick-Play" className="mr-4 select-none">
 					速攻魔法
 				</label>
-				<input type="checkbox" id="Ritual" name="frame-types[]" value="Ritual" />
+				<input type="checkbox" id="Ritual" name="play-types[]" value="Ritual" />
 				<label htmlFor="Ritual" className="mr-4 select-none">
 					儀式魔法
 				</label>
--- resources/js/Components/Admin/Filters/TrapCardFilter.jsx
+++ resources/js/Components/Admin/Filters/TrapCardFilter.jsx
@@ -5,15 +5,15 @@ function TrapCardFilter() {
 		<div className="mt-2 flex justify-start items-center">
 			<p className="w-28 text-center">種類</p>
 			<div className="ml-2">
-				<input type="checkbox" id="Normal" name="frame-types[]" value="Normal" />
+				<input type="checkbox" id="Normal" name="play-types[]" value="Normal" />
 				<label htmlFor="Normal" className="mr-4 select-none">
 					通常罠
 				</label>
-				<input type="checkbox" id="Continuous" name="frame-types[]" value="Continuous" />
+				<input type="checkbox" id="Continuous" name="play-types[]" value="Continuous" />
 				<label htmlFor="Continuous" className="mr-4 select-none">
 					永続罠
 				</label>
-				<input type="checkbox" id="Counter" name="frame-types[]" value="Counter" />
+				<input type="checkbox" id="Counter" name="play-types[]" value="Counter" />
 				<label htmlFor="Counter" className="mr-4 select-none">
 					カウンター罠
 				</label>
