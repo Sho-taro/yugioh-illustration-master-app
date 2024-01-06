418a5d4799e0c50380854280601f6f576041a9e1
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-01-06 23:19:11 +0900

    [add]Add new feature
    canvasアニメーションを一時停止できる機能を追加

--- resources/js/Components/Gallery/CanvasMaskModal.jsx
+++ resources/js/Components/Gallery/CanvasMaskModal.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Link } from '@inertiajs/react';
 
-function CanvasMenuBar() {
+function CanvasMenuBar({handleClick}) {
   return (
 		<>
 			<div className="canvas-menu-bar">
 				<div className="h-full flex justify-end items-center">
 					<div className="mr-8">
+						<button
+							onClick={handleClick}
+							className="text-xl text-white font-bold">
+							pause/restart
+						</button>
+					</div>
+					<div className="mr-8">
 						<button className="text-xl text-white font-bold">設定</button>
 					</div>
 					<div className="mr-8">
-						<Link href={route('gallery.setting')} className="text-xl text-white font-bold">終了</Link>
+						<a
+							href={route('gallery.setting')}
+							className="text-xl text-white font-bold">
+							終了
+						</a>
 					</div>
 				</div>
 			</div>
