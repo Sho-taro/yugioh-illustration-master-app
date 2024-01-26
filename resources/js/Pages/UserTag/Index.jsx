afab929d024a1a1ecba235d0372a266f4ac1b711
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-01-26 11:50:40 +0900

    [fix]Fix bag
    refs #140 userTagsが0件のとき、表示件数の最小値が1になってしまう不具合を解消

--- resources/js/Pages/UserTag/Index.jsx
+++ resources/js/Pages/UserTag/Index.jsx
@@ -11,10 +11,15 @@ import AddIcon from '@mui/icons-material/Add';
 import UserTagThumbnail from '@/Components/UserTags/UserTagThumbnail';
 
 function Index({ auth, userTagsNum, userTagsData, message }) {
-	let showingMaxIndex;
+	let showingMinIndex; // 枚数表示の最小値
+	userTagsNum === 0
+		? (showingMinIndex = 0)
+		: (showingMinIndex = 1 + (userTagsData.current_page - 1) * 5);
+	let showingMaxIndex; // 枚数表示の最大値
 	userTagsData.current_page * 5 >= userTagsNum
 		? (showingMaxIndex = userTagsNum)
 		: (showingMaxIndex = userTagsData.current_page * 5);
+
 	return (
 		<>
 			<Header auth={auth} needOnlyLogo={true} />
@@ -33,8 +38,8 @@ function Index({ auth, userTagsNum, userTagsData, message }) {
 					<div className="mb-2 flex justify-between items-end">
 						<p>
 							全 <span className="text-xl font-bold">{userTagsNum}</span> 件 （
-							{1 + (userTagsData.current_page - 1) * 5} -{' '}
-							{showingMaxIndex } 件を表示中 ）
+							{showingMinIndex} - {showingMaxIndex} 件を表示中
+							）
 						</p>
 						<Link href={`/tags/${auth.user.id}/create`}>
 							<Button
