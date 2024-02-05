07bae5196d8385b8ba9cb1c331f81009a7b391ad
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-02-05 12:22:37 +0900

    [modify]Modify layout
    refs #140 MyTagModeページのレイアウトを修正

--- resources/js/Pages/Gallery/MyTagMode.jsx
+++ resources/js/Pages/Gallery/MyTagMode.jsx
@@ -13,25 +13,22 @@ import TooltipBackButton from '@/Components/MaterialUI/TooltipBackButton';
 
 function MyTagMode({ auth, userTags, errorMsg }) {
 	const [targetUserTagId, setTargetUserTagId] = useState(null);
-	const targetUTClassName = 'mb-8 border-4 border-green-800 rounded-md cursor-pointer relative';
-  const notTargetUTClassName = 'mb-8 cursor-pointer';
-  const handleBtnClick = () => {
-    if (!targetUserTagId) return;
-    router.get(route('gallery.userTag'), {
-      'user-tag-id': targetUserTagId,
-    });
-  }
+	const targetUTClassName = 'mb-8 border-4 border-green-800 rounded-xl relative';
+	const notTargetUTClassName = 'mb-8 border-4 border-gray-800 rounded-xl cursor-pointer relative';
+	const handleBtnClick = () => {
+		if (!targetUserTagId) return;
+		router.get(route('gallery.userTag'), {
+			'user-tag-id': targetUserTagId,
+		});
+	};
 
 	return (
 		<>
 			<Header auth={auth} needOnlyLogo={true} />
 			<div className="w-3/5 mx-auto">
 				<TooltipBackButton href={route('index')} />
-				<div className="max-w-fit mx-auto" style={{ minWidth: '80%' }}>
-					<Typography
-						variant="h5"
-						component="h2"
-						sx={{ textAlign: 'center', fontWeight: 700, mb: '1rem' }}>
+				<div className="max-w-fit mx-auto text-center" style={{ minWidth: '80%' }}>
+					<Typography variant="h5" component="h2" sx={{ fontWeight: 700, mb: '1rem' }}>
 						MyTagモード
 					</Typography>
 					<Typography variant="p" component="p">
@@ -62,11 +59,18 @@ function MyTagMode({ auth, userTags, errorMsg }) {
 									}
 									onClick={() => setTargetUserTagId(userTag.id)}>
 									<UserTagThumbnail userTag={userTag} mapIndex={mapIndex} />
-									{targetUserTagId === userTag.id && (
+									{targetUserTagId === userTag.id ? (
 										<div className="absolute top-1/2 left-0 -translate-x-12 -translate-y-1/2">
 											<CheckCircleRoundedIcon
 												fontSize="large"
 												sx={{ color: '#065F46' }}
+											/>
+										</div>
+									) : (
+										<div className="absolute top-1/2 left-0 -translate-x-12 -translate-y-1/2">
+											<CheckCircleRoundedIcon
+												fontSize="large"
+												sx={{ color: 'gray', opacity: 0.1 }}
 											/>
 										</div>
 									)}
@@ -79,7 +83,16 @@ function MyTagMode({ auth, userTags, errorMsg }) {
 						</p>
 					)}
 
-					<div className="mb-8 max-w-fit mx-auto">
+					<div className="mb-8 text-center w-fit mx-auto">
+						{!targetUserTagId && (
+							<Typography
+								variant="p"
+								component="p"
+								sx={{ mb: '1rem' }}
+								style={{ color: 'red' }}>
+								MyTagが選択されていません。
+							</Typography>
+						)}
 						<Button
 							variant="contained"
 							color="error"
@@ -88,7 +101,7 @@ function MyTagMode({ auth, userTags, errorMsg }) {
 							disabled={!targetUserTagId}
 							onClick={handleBtnClick}
 							sx={{ textTransform: 'none' }}>
-							<span className="text-white">スタート</span>
+							スタート
 						</Button>
 					</div>
 				</div>
