cde437f13604979dd44f7181c3c29ee031a53d0b
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-02-09 19:28:28 +0900

    [add]Add new feature
    footerを作成し、Layoutに追加

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
 import { Head } from '@inertiajs/react';
 
-function Layout({title, children}) {
-  return (
+function Layout({ title, children }) {
+	return (
 		<>
 			<Head title={title ? title : '遊戯王イラストクイズ'} />
 			{/* <Head>
         <title>{title}</title>
       </Head> */}
-			<div className="layout-bg min-h-full p-2">
+			<div className="layout-bg min-h-full p-2 relative">
 				{children}
+				<div className="mt-16 mb-4 flex justify-center">
+					<small className="text-gray-500"> Copyright &copy; 2024 sk</small>
+				</div>
 			</div>
 		</>
-  );
+	);
 }
 
-export default Layout
+export default Layout;
