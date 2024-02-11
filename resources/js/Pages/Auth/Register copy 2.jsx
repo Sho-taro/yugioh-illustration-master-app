0fcf4e5d10d3ae7f300ab48ec713885a1be31f27
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-02-11 17:44:57 +0900

    [modify]Modify layout
    refs #149 ユーザー登録画面とログイン画面のレイアウトを変更

---
+++ resources/js/Pages/Auth/Register copy 2.jsx
@@ -0,0 +1,203 @@
+import { useEffect } from 'react';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Link, useForm } from '@inertiajs/react';
+import imageSrcs from '@/utils/authImages';
+import { shuffleArray } from '@/utils/shuffleArray';
+import GalleryLayout from '@/Layouts/GalleryLayout';
+
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TooltipBackButton from '@/Components/MaterialUI/TooltipBackButton';
+
+export default function RegisterCustomize() {
+	const { data, setData, post, processing, errors, reset } = useForm({
+		name: '',
+		email: '',
+		password: '',
+		password_confirmation: '',
+	});
+	const imageSrcsNew = shuffleArray(imageSrcs);
+	console.log(imageSrcsNew);
+
+	useEffect(() => {
+		return () => {
+			reset('password', 'password_confirmation');
+		};
+	}, []);
+
+	const handleBtnClick = e => {
+		post(route('register'));
+	};
+
+	return (
+		<div className="flex min-h-screen">
+			{/* <div className="w-1/2 flex flex-col justify-around">
+				<div className="grid grid-cols-4">
+					{imageSrcsNew[0].map((src, i) => (
+						<div key={i} className="mx-1">
+							<img src={src} alt="イラスト" />
+						</div>
+					))}
+				</div>
+				<div className="grid grid-cols-4">
+					{imageSrcsNew[1].map((src, i) => (
+						<div key={i} className="mx-1">
+							<img src={src} alt="イラスト" />
+						</div>
+					))}
+				</div>
+				<div className="grid grid-cols-4">
+					{imageSrcsNew[2].map((src, i) => (
+						<div key={i} className="mx-1">
+							<img src={src} alt="イラスト" />
+						</div>
+					))}
+				</div>
+				<div className="grid grid-cols-4">
+					{imageSrcsNew[3].map((src, i) => (
+						<div key={i} className="mx-1">
+							<img src={src} alt="イラスト" />
+						</div>
+					))}
+				</div>
+			</div> */}
+			<div className="w-1/2 flex justify-center items-center">
+				<div className="w-2/3">
+					<img
+						src="/images/card-images/agov-jp006.jpg"
+						alt="イラスト"
+						className="rounded-md"
+					/>
+				</div>
+			</div>
+			<div className="w-1/2 flex justify-center items-center border-l border-gray-700">
+				<div className="w-1/2 mx-auto">
+					<TooltipBackButton href={route('index')} />
+					<Typography
+						variant="h5"
+						className="text-center"
+						sx={{ mt: '1.5rem', mb: '0.5rem' }}>
+						ユーザー登録
+					</Typography>
+					<form>
+						<div>
+							<InputLabel htmlFor="name" value="ユーザー名" className="text-white" />
+
+							<TextInput
+								id="name"
+								name="name"
+								value={data.name}
+								className="mt-1 block w-full"
+								autoComplete="name"
+								isFocused={true}
+								onChange={e => setData('name', e.target.value)}
+								required
+							/>
+
+							<InputError
+								message={errors.name}
+								className="mt-2"
+								style={{ color: 'red' }}
+							/>
+						</div>
+
+						<div className="mt-4">
+							<InputLabel
+								htmlFor="email"
+								value="メールアドレス"
+								className="text-white"
+							/>
+
+							<TextInput
+								id="email"
+								type="email"
+								name="email"
+								value={data.email}
+								className="mt-1 block w-full"
+								autoComplete="username"
+								onChange={e => setData('email', e.target.value)}
+								required
+							/>
+
+							<InputError
+								message={errors.email}
+								className="mt-2"
+								style={{ color: 'red' }}
+							/>
+						</div>
+
+						<div className="mt-4">
+							<InputLabel
+								htmlFor="password"
+								value="パスワード（半角英数字8文字以上）"
+								className="text-white"
+							/>
+
+							<TextInput
+								id="password"
+								type="password"
+								name="password"
+								value={data.password}
+								className="mt-1 block w-full"
+								autoComplete="new-password"
+								onChange={e => setData('password', e.target.value)}
+								required
+							/>
+
+							<InputError
+								message={errors.password}
+								className="mt-2"
+								style={{ color: 'red' }}
+							/>
+						</div>
+
+						<div className="my-4">
+							<InputLabel
+								htmlFor="password_confirmation"
+								value="パスワード再入力"
+								className="text-white"
+							/>
+
+							<TextInput
+								id="password_confirmation"
+								type="password"
+								name="password_confirmation"
+								value={data.password_confirmation}
+								className="mt-1 block w-full"
+								autoComplete="new-password"
+								onChange={e => setData('password_confirmation', e.target.value)}
+								required
+							/>
+
+							<InputError
+								message={errors.password_confirmation}
+								className="mt-2"
+								style={{ color: 'red' }}
+							/>
+						</div>
+						<div className="flex flex-col items-center justify-center mt-12">
+							<Button
+								variant="contained"
+								color="error"
+								size="large"
+								disableRipple
+								disabled={processing}
+								onClick={handleBtnClick}>
+								新規登録
+							</Button>
+							<Link
+								href={route('login')}
+								className="block mt-6 text-gray-500 underline">
+								既に登録済みの方はこちら
+							</Link>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
+RegisterCustomize.layout = page => <GalleryLayout title="ユーザー登録" children={page} />;
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,116 +1,152 @@
 import { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
+// import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { Link, useForm } from '@inertiajs/react';
+import GalleryLayout from '@/Layouts/GalleryLayout';
+
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TooltipBackButton from '@/Components/MaterialUI/TooltipBackButton';
 
 export default function Register() {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-    });
+	const { data, setData, post, processing, errors, reset } = useForm({
+		name: '',
+		email: '',
+		password: '',
+		password_confirmation: '',
+	});
 
-    useEffect(() => {
-        return () => {
-            reset('password', 'password_confirmation');
-        };
-    }, []);
+	useEffect(() => {
+		return () => {
+			reset('password', 'password_confirmation');
+		};
+	}, []);
 
-    const submit = (e) => {
-        e.preventDefault();
+	const handleBtnClick = e => {
+		post(route('register'));
+	};
 
-        post(route('register'));
-    };
+	return (
+		<div className="min-h-screen flex justify-center items-center">
+			<div className="w-1/4 mx-auto">
+				<TooltipBackButton href={route('index')} title="Topページへ戻る" />
+				<Typography
+					variant="h5"
+					className="text-center"
+					sx={{ mt: '1.5rem', mb: '0.5rem', fontWeight: '700' }}>
+					ユーザー登録
+				</Typography>
+				<form>
+					<div>
+						<InputLabel htmlFor="name" value="ユーザー名" className="text-white" />
 
-    return (
-		<GuestLayout>
-			<Head title="新規登録" />
+						<TextInput
+							id="name"
+							name="name"
+							value={data.name}
+							className="mt-1 block w-full"
+							autoComplete="name"
+							isFocused={true}
+							onChange={e => setData('name', e.target.value)}
+							required
+						/>
 
-			<form onSubmit={submit}>
-				<div>
-					<InputLabel htmlFor="name" value="ユーザー名" />
+						<InputError
+							message={errors.name}
+							className="mt-2"
+							style={{ color: 'red' }}
+						/>
+					</div>
 
-					<TextInput
-						id="name"
-						name="name"
-						value={data.name}
-						className="mt-1 block w-full"
-						autoComplete="name"
-						isFocused={true}
-						onChange={e => setData('name', e.target.value)}
-						required
-					/>
+					<div className="mt-4">
+						<InputLabel htmlFor="email" value="メールアドレス" className="text-white" />
 
-					<InputError message={errors.name} className="mt-2" />
-				</div>
+						<TextInput
+							id="email"
+							type="email"
+							name="email"
+							value={data.email}
+							className="mt-1 block w-full"
+							autoComplete="username"
+							onChange={e => setData('email', e.target.value)}
+							required
+						/>
 
-				<div className="mt-4">
-					<InputLabel htmlFor="email" value="メールアドレス" />
+						<InputError
+							message={errors.email}
+							className="mt-2"
+							style={{ color: 'red' }}
+						/>
+					</div>
 
-					<TextInput
-						id="email"
-						type="email"
-						name="email"
-						value={data.email}
-						className="mt-1 block w-full"
-						autoComplete="username"
-						onChange={e => setData('email', e.target.value)}
-						required
-					/>
+					<div className="mt-4">
+						<InputLabel
+							htmlFor="password"
+							value="パスワード（半角英数字8文字以上）"
+							className="text-white"
+						/>
 
-					<InputError message={errors.email} className="mt-2" />
-				</div>
+						<TextInput
+							id="password"
+							type="password"
+							name="password"
+							value={data.password}
+							className="mt-1 block w-full"
+							autoComplete="new-password"
+							onChange={e => setData('password', e.target.value)}
+							required
+						/>
 
-				<div className="mt-4">
-					<InputLabel htmlFor="password" value="パスワード（半角英数字8文字以上）" />
+						<InputError
+							message={errors.password}
+							className="mt-2"
+							style={{ color: 'red' }}
+						/>
+					</div>
 
-					<TextInput
-						id="password"
-						type="password"
-						name="password"
-						value={data.password}
-						className="mt-1 block w-full"
-						autoComplete="new-password"
-						onChange={e => setData('password', e.target.value)}
-						required
-					/>
+					<div className="my-4">
+						<InputLabel
+							htmlFor="password_confirmation"
+							value="パスワード再入力"
+							className="text-white"
+						/>
 
-					<InputError message={errors.password} className="mt-2" />
-				</div>
+						<TextInput
+							id="password_confirmation"
+							type="password"
+							name="password_confirmation"
+							value={data.password_confirmation}
+							className="mt-1 block w-full"
+							autoComplete="new-password"
+							onChange={e => setData('password_confirmation', e.target.value)}
+							required
+						/>
 
-				<div className="my-4">
-					<InputLabel htmlFor="password_confirmation" value="パスワード再入力" />
-
-					<TextInput
-						id="password_confirmation"
-						type="password"
-						name="password_confirmation"
-						value={data.password_confirmation}
-						className="mt-1 block w-full"
-						autoComplete="new-password"
-						onChange={e => setData('password_confirmation', e.target.value)}
-						required
-					/>
-
-					<InputError message={errors.password_confirmation} className="mt-2" />
-				</div>
-				<Link
-					href={route('login')}
-					className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-					既に登録済みの方はこちら
-				</Link>
-
-				<div className="flex flex-col items-center justify-center mt-8">
-					<PrimaryButton className="" disabled={processing}>
-						新規登録
-					</PrimaryButton>
-					<Link href={route('index')} className="block mt-4 underline">または 登録せずに戻る</Link>
-				</div>
-			</form>
-		</GuestLayout>
+						<InputError
+							message={errors.password_confirmation}
+							className="mt-2"
+							style={{ color: 'red' }}
+						/>
+					</div>
+					<div className="flex flex-col items-center justify-center mt-12">
+						<Button
+							variant="contained"
+							color="error"
+							size="large"
+							disableRipple
+							disabled={processing}
+							onClick={handleBtnClick}>
+							新規登録
+						</Button>
+						<Link href={route('login')} className="block mt-6 text-gray-500 underline">
+							既に登録済みの方はこちら
+						</Link>
+					</div>
+				</form>
+			</div>
+		</div>
 	);
 }
+Register.layout = page => <GalleryLayout title="ユーザー登録" children={page} />;
