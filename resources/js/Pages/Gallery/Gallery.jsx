9dfb24ec3713da7b72425fb94654eb17e7040038
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-02-21 18:00:49 +0900

    [add]Add new feature
    refs #155 スマートフォン用のcanvasアニメーションを実装

--- resources/js/Pages/Gallery/Gallery.jsx
+++ resources/js/Pages/Gallery/Gallery.jsx
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import Canvas from '@/Components/Gallery/Canvas';
+import CanvasSP from '@/Components/Gallery/CanvasSP';
 import GalleryLayout from '@/Layouts/GalleryLayout';
 
 function Gallery({ cards }) {
 	const [animationState, setAnimationState] = useState('waiting');
 	const canvasCards = [];
+	const windowWidth = window.innerWidth;
 
 	return (
 		<>
-			<Canvas
-				cards={cards}
-				animationState={animationState}
-				setAnimationState={setAnimationState}
-				canvasCards={canvasCards}
-			/>
+			{windowWidth > 640 ? (
+				<Canvas
+					cards={cards}
+					animationState={animationState}
+					setAnimationState={setAnimationState}
+					canvasCards={canvasCards}
+				/>
+			) : (
+				<CanvasSP
+					cards={cards}
+					animationState={animationState}
+					setAnimationState={setAnimationState}
+					canvasCards={canvasCards}
+				/>
+			)}
 		</>
 	);
 }
