27c7b8143d40d56c00e1b4605ff3124d39dd5e9a
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-03-03 03:01:27 +0900

    [add]Add feature test
    refs #162 Myタグを一覧表示する機能の機能テストを実装

--- database/factories/UserTagFactory.php
+++ database/factories/UserTagFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserTag>
@@ -17,8 +18,8 @@ class UserTagFactory extends Factory
     public function definition(): array
     {
         return [
-            // 'user_id' => 1,
-            'name' => fake()->name(),
+            'user_id' => User::factory(),
+            'name' => $this->faker->unique()->word(),
             'status' => 'public',
             'popularity' => 0,
         ];
