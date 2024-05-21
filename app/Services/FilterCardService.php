ca8cb4cb016072325cf9723e5babda90ff81a0ec
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-05-21 14:26:48 +0900

    [modify] refactoring code
    refs #176 全カードのレコードを取得するためのクエリをビルドするコードを修正

--- app/Services/FilterCardService.php
+++ app/Services/FilterCardService.php
@@ -314,4 +314,60 @@ class FilterCardService {
 
     return $releasedCards_query;
   }
+
+
+  /**
+   * released_cardsテーブルからレコードを取得するためのSQL分を組み立てる（all用）
+   * 「ユーザが入力した絞り込み条件に合致したカードの情報のみをDBから取得する」ためのクエリをビルドする
+   *
+   * @param array $filters
+   * @return
+   */
+  public function buildReleasedCardsQueryForAll(array $filters)
+  {
+    if ($filters['target'] !== 'all') return false;
+
+    // released_cardsテーブルのクエリビルダインスタンスを取得
+    $releasedCards_query = DB::table('released_cards');
+
+    $releasedCards_query
+      ->select(
+        'released_cards.id as released_card_id',
+        'cards.id as card_id',
+        'released_cards.product_code',
+        'released_cards.list_number',
+        'products.name_ja as product_ja',
+        'products.name_en as product_en',
+        'products.release_date',
+        'periods.name as period',
+        'cards.name_ja as card_ja',
+        'cards.name_ja_kana as card_ja_kana',
+        'cards.name_en as card_en',
+        'frame_types.name_ja as frame_type_ja',
+        'frame_types.name_en as frame_type_en',
+        // 'archetypes.name_ja as archetype',   // 今は不要
+        'attributes.name_ja as attribute',
+        'monster_card_details.attack',
+        'races.name_ja as race',
+        'attributes.name_ja as attribute',
+        'monster_card_details.level_or_rank',
+        'monster_card_details.link_value',
+        'spell_trap_play_types.name_ja as play_type',
+      )
+      ->join('products', 'released_cards.product_code', '=', 'products.product_code')
+      ->join('periods', function (JoinClause $join) {
+        $join->on('products.release_date', '>=', 'periods.start_date')
+              ->on('products.release_date', '<=', 'periods.end_date');
+      })
+      ->join('cards', 'released_cards.card_official_id', '=', 'cards.card_official_id')
+      ->join('frame_types', 'cards.frame_type_code', '=', 'frame_types.frame_type_code')
+      // ->join('archetypes', 'cards.archetype_code', '=', 'archetypes.archetype_code')   // 今は不要
+      ->leftJoin('monster_card_details', 'cards.card_official_id', '=', 'monster_card_details.card_official_id')
+      ->leftJoin('races', 'monster_card_details.race_code', '=', 'races.race_code')
+      ->leftJoin('attributes', 'monster_card_details.attribute_code', '=', 'attributes.attribute_code')
+      ->leftJoin('spell_trap_card_details', 'cards.card_official_id', '=', 'spell_trap_card_details.card_official_id')
+      ->leftJoin('spell_trap_play_types', 'spell_trap_card_details.play_type_code', '=', 'spell_trap_play_types.play_type_code');
+
+    return $releasedCards_query;
+  }
 }
